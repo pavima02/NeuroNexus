033eb7c949c2e3498245806b8c2ec74f176c2ea1
pavithra <120707313+pavima02@users.noreply.github.com>
2025-04-30 13:01:41 +0530

    Add files via upload

---
+++ passwordgenerator.py
@@ -0,0 +1,17 @@
+import random
+import string
+
+def generate_password(length):
+    characters = string.ascii_letters + string.digits + string.punctuation
+    password = ''.join(random.choice(characters) for _ in range(length))
+    return password
+
+try:
+    length = int(input("Enter desired password length: "))
+    if length <= 0:
+        print("Please enter a positive number.")
+    else:
+        password = generate_password(length)
+        print("Generated Password:", password)
+except ValueError:
+    print("Invalid input. Please enter a number.")
